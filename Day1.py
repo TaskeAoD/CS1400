e8aa2f4ecc3c76779fe33bdeb2fbcb71c349e4d4
TaskeAoD <taskehimara@gmail.com>
2025-01-09 11:21:29 -0700

    CodeAlong1 Percentage

--- Day1.py
+++ Day1.py
@@ -12,3 +12,33 @@ print(x)
 print(y)
 print(z)
 print('x=', x,'y=', y, 'z=',z)
+
+#inputting variables into a set of strings
+a = 10
+print("first, variable 'a' has a value", a, "and a type", type(a))
+b = 'ABC'
+print("first, variable 'a' has a value", a, "and a type", type(a))
+print(b)
+
+x = 5.654
+print(x)
+print(type(x))
+
+pi =  3.14159
+print(pi)
+a = 2.72343464567567
+print(a)
+print(int(a))
+print(round(a))
+print(round(a,4))
+
+d = 12345
+print(round(d,-2))
+
+#print('Please input something here: ')
+#x = input()
+#print('you input: ', x)
+
+print('Please input something here: ')
+x = int(input())
+print(x)
