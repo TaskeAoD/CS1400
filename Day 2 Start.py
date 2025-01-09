24f774b883e3616631686fe229abdb9bc456a194
TaskeAoD <taskehimara@gmail.com>
2025-01-09 08:51:06 -0700

    Day 2 test

---
+++ Day 2 Start.py
@@ -0,0 +1,7 @@
+radius = float(input("Enter the radius of a circle: "))
+area = 3.14 * radius + radius
+print("The area of a circle is: ", area)
+
+square = float(input("How about a square: "))
+areasq = square * square
+print("The area of a square is : ", areasq)
