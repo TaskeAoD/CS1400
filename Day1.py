494f1e3cde40d9005a46fbd21afe2a0b8cf873fc
TaskeAoD <taskehimara@gmail.com>
2025-01-09 09:19:17 -0700

    In class exercise on variables

--- Day1.py
+++ Day1.py
@@ -1,5 +1,14 @@
-print('Michael Lund')
-print('1+2')
-print(1+2)
-input()
-print(input)
+#Normal variable
+x=10
+print('x = ',x)
+x=20
+print('x = ',x)
+x=30
+print('x = ',x)
+
+#Tupples(mulitple variables on one line)
+x, y, z = 100, -45, 0
+print(x)
+print(y)
+print(z)
+print('x=', x,'y=', y, 'z=',z)
