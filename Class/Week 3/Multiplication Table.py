a4d550b13bc105401d7fe3204157834b13337e48
TaskeAoD <taskehimara@gmail.com>
2025-01-23 10:08:48 -0700

    More formatting

--- Class/Week 3/Multiplication Table.py
+++ Class/Week 3/Multiplication Table.py
@@ -1,7 +1,16 @@
 # get multiplication table size from user
 size = int(input('Please enter the table size: '))
 # Print a size by size multiplication table
+
+for column in range(1, size+1):
+    print('{0:4}'.format(column), end='')
+print()
+
+for column in range(1, size+1):
+    print('-----', end='')
+print()
 for row in range(1, size+1):
+    #print(row#, row)
     for column in range(1, size+1):
         product = row*column #computes the product
         print('{0:4}'.format(product), end='') #Displays product
