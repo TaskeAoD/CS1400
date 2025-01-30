e8d9cc3c91d89a1b3db144d138bfed3eb10dc261
TaskeAoD <taskehimara@gmail.com>
2025-01-30 10:16:22 -0700

    Added random and rounding

--- Class/Week 4/Triangle Calculator.py
+++ Class/Week 4/Triangle Calculator.py
@@ -1,4 +1,5 @@
 from math import sqrt, pow
+from random import randint
 
 sideA = float(input("Enter the value for Side A: "))
 sideB = float(input("Enter the value for Side B: "))
@@ -10,6 +11,19 @@ sideA_Squared = pow(sideA, 2)
 sideB_Squared = pow(sideB, 2)
 
 sideC_Squared = sideA_Squared + sideB_Squared
+sideC = sqrt(sideC_Squared)
+print(f"The value of Side C for your numbers is {round(sideC, 2)}")
+
+sideA = randint(1, 101)
+sideB = randint(1, 101)
+
+print(f"The random value entered for Side A is {sideA}")
+print(f"The random value entered for Side B is {sideB}")
+
+sideA_Squared = pow(sideA, 2)
+sideB_Squared = pow(sideB, 2)
+
+sideC_Squared = sideA_Squared + sideB_Squared
 
 sideC = sqrt(sideC_Squared)
-print(f"The value of Side C is {sideC}")
+print(f"The value of Side C for the random values is {round(sideC, 2)}")
