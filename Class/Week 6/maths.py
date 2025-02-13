7b92313578c7c432adc5b1ce323fd0e46ed8110a
TaskeAoD <taskehimara@gmail.com>
2025-02-13 09:18:34 -0700

    CLass

---
+++ Class/Week 6/maths.py
@@ -0,0 +1,16 @@
+def add(x, y):
+    '''
+    adds x,y
+    '''
+    return(x + y)
+
+def multiply(x, y):
+    '''
+    multiply x,y
+    '''
+    return(x * y)
+def evaluate(f, x, y):
+    '''
+    calls the function f with parameters x, y
+    f(x, y)'''
+    return f(x, y)
