b8b6bfe21efa7d65e6230c792a3c22823d6e8902
TaskeAoD <taskehimara@gmail.com>
2025-02-25 09:20:06 -0700

    Added Exceptions 2 and 3

--- Class/Week 8/#Multiple Exceptions3.py
+++ Class/Week 8/#Multiple Exceptions3.py
@@ -15,15 +15,15 @@ for i in range(10):
             print({}[1])
         else:
             print(3/0)
-    except ValueError:
-        print("Cannot convert integer")
-    except IndexError:
-        print("List index out of range.")
-    except ZeroDivisionError:
-        print("Cannot divide by 0")
-    except Exception: #mostly a catch all
-        print("There was another type of error.")
+    except ValueError as e:
+        print("Cannot convert integer", type(e), e)
+    except IndexError as e:
+        print("List index out of range.", type(e), e)
+    except ZeroDivisionError as e:
+        print("Cannot divide by 0", type(e), e)
+    except Exception as e: #mostly a catch all
+        print("There was another type of error.", type(e), e)
     except: #This is the real catch all, has to be the last except block in the try group
-        print("There was another unexpected error")
+        print("There was another unexpected error", type(e), e)
         
     print(f"End of loop iteration: {i}")
