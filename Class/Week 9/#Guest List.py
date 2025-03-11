2a13ac7fffa27be9a9089f3d3b956e2d25242141
TaskeAoD <taskehimara@gmail.com>
2025-03-11 10:09:42 -0600

    added open to guest

--- Class/Week 9/#Guest List.py
+++ Class/Week 9/#Guest List.py
@@ -2,9 +2,15 @@
 
 from pathlib import Path
 
-contents = input("Please enter First Name: ")
-contents += ' ' #You need the += so it adds instead of overwrites the previous
-contents += input("Please enter Last Name: ") 
+firstname = input("Please enter First Name: \n")
+space = ' ' #You need the += so it adds instead of overwrites the previous
+lastname = input("Please enter Last Name: \n")
+#fullName = (firstname + space + lastname)
+fullName = f'{firstname} {lastname}\n'
 
 path = Path("C:/Users/Taske/Documents/Github/CS1400/Class/Week 9/Guest.txt")
-path.write_text(contents)
+path.write_text(fullName)
+
+with open(path) as file_object:
+    for line in file_object:
+        print(line)
