ee61389cd7fffe55bc2a0c32e8fdae3e41236032
TaskeAoD <taskehimara@gmail.com>
2025-03-11 09:58:53 -0600

    removed new line from guest

--- Class/Week 9/#Guest List.py
+++ Class/Week 9/#Guest List.py
@@ -2,9 +2,9 @@
 
 from pathlib import Path
 
-contents = input("Please enter First Name: \n")
+contents = input("Please enter First Name: ")
 contents += ' ' #You need the += so it adds instead of overwrites the previous
-contents += input("Please enter Last Name: \n") 
+contents += input("Please enter Last Name: ") 
 
 path = Path("C:/Users/Taske/Documents/Github/CS1400/Class/Week 9/Guest.txt")
 path.write_text(contents)
