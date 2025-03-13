85e0ae9b4affb0f90d89a725dda1fc281f90a4b0
TaskeAoD <taskehimara@gmail.com>
2025-03-13 10:12:58 -0600

    Multiple Changes before turn in

--- Class/Week 9/#Code Along Week 9.py
+++ Class/Week 9/#Code Along Week 9.py
@@ -1,4 +1,4 @@
-#Code Along Week 9, l# = Line number comment is attached to due to tab lining and as a countermeasure to copying
+#Code Along Week 9
 '''Shopping list
 Add Items, Remove Items, View List
 Read, write, open, close files'''
@@ -9,41 +9,42 @@ def LoadList():
     print(f"Loading {file}")
     
     try:
-        f = open(f"{file}", 'r')                #Opening file in 'read' mode l12
+        f = open(f"{file}", 'r')                #Opening file in 'read' mode
         for line in f:
             line = line.strip()
             list.append(line)
-        f.close()                               #This will close the file l16
+        f.close()                               #This will close the file
     except FileNotFoundError as err:
         print(err)
         print(f"Creating {file}")
-        f = open(f"{file}", 'w')                #This will create the file if it doesn't exist already l20
+        f = open(f"{file}", 'w')                #This will create the file if it doesn't exist already
         f.close()
 
 def AddItem():
     item = input("What Item do you want to add to the list? ")
-    list.append(item)                           #Appends list with item l25
+    list.append(item)                           #Appends list with item
     
     try:
         print(f"Adding {item} to {file}")
-        f =  open(f"{file}", 'a')               #Opens Groceries.txt to append the item to the list l28
-        f.write(item + "\n")                    #Adds item to list, the creates new line l29
-        f.close()                               #Closes file l30
+        f =  open(f"{file}", 'a')               #Opens Groceries.txt to append the item to the list
+        f.write(item + "\n")                    #Adds item to list, the creates new line
+        f.close()                               #Closes file
         
     except FileNotFoundError as err:
         print(err)
         
 def ViewList():
     print(f"Here are the items in your {file} list:")
-    for i in list:                              #Iterates list through each item l38
-        print(i)                                #Prints each item found in list l39
+    for i in list:                              #Iterates list through each item
+        print(i)                                #Prints each item found in list
+        SaveList()
         
 def SaveList():
     try:
         f = open(file, 'w')
-        for i in list:                          #For each item in the list write to file l44
-            f.write(i + "\n")                   #Write each item into the file on a new line l45
-        f.close()                               #Close the file after all items are added to the file l46
+        for i in list:                          #For each item in the list write to file
+            f.write(i + "\n")                   #Write each item into the file on a new line
+        f.close()                               #Close the file after all items are added to the file
         
     except FileNotFoundError as err:
         print(err)
@@ -51,7 +52,7 @@ def SaveList():
 def RemoveItems():
     try:
         item = input(f"What item would you like to remove from {file}? ")
-        list.remove(item)                       #Removes Item from list l54
+        list.remove(item)                       #Removes Item from list l55
         ViewList()
         SaveList()
         
@@ -61,11 +62,30 @@ def RemoveItems():
         
     
 def Menu():
-    
-    
+    print(f"1: Add item to {file}")
+    print(f"2: Remove item from {file}")
+    print(f"3: Show items in {file}")
+    print("4: Quit program")
 
-LoadList()
-AddItem()
-ViewList()
-#SaveList()
-RemoveItems()
+
+choice = 0    
+while choice != 4:
+    ViewList()
+    Menu()
+    try:
+        choice = int(input("Make a choice from the above options: "))
+        if choice == 1:
+            AddItem()
+        if choice == 2:
+            RemoveItems()
+        if choice == 3:
+            ViewList()
+        if choice == 4:
+            print("Thank you for using the List Generator!")
+            break
+        else:
+            raise Exception ("Not a valid number.")
+    except ValueError as err:
+        print("Please enter an integer between 1 - 4")
+    except:
+        print("Please enter an integer between 1 - 4")
