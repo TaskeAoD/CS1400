b812874c15d5e4b654438fde69c9722c1e66375b
TaskeAoD <taskehimara@gmail.com>
2025-03-25 09:12:49 -0600

    Downloads for week 11

---
+++ Class/Week 11/Code Download/testdigitalsw.py
@@ -0,0 +1,10 @@
+#testdigitalsw.py
+
+from digitalStopWatch import DigitalStopwatch
+from time import sleep
+
+dsw = DigitalStopwatch()
+dsw.start() # Start the timer
+sleep(140) # Do noting for 2:20
+dsw.stop() # Stop the timer
+print(dsw.digital_time()) # Print elapsed time in digital format
---
+++ Class/Week 11/Code Download/top.py
@@ -0,0 +1,5 @@
+#top.py
+
+class Top(object):
+    def __init__(self, **kwargs):
+        pass # Terminate the constructor call chain
