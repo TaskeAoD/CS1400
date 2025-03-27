497498f668f21e57bce26e09a7551f27484dc549
TaskeAoD <taskehimara@gmail.com>
2025-03-27 08:50:09 -0600

    Downloaded for Code Along

---
+++ Class/Week 11/Code Along/CodeAlong11/CodeAlong11.py
@@ -0,0 +1,46 @@
+# pygame demo 8  - SimpleText, SimpleButton, and Ball
+
+# 1 - Import packages
+from Ball import Ball
+from SimpleButton import SimpleButton
+from SimpleText import SimpleText
+import time
+
+# 2 - Define constants
+
+       
+# 3 - Initialize the world
+
+
+# 4 - Load assets: image(s), sounds, etc.
+
+# 5 - Initialize variables
+
+
+# 6 - Loop forever
+
+    
+    # 7 - Check for and handle events
+
+
+
+
+					# clicked button, reset counter
+
+    # 8 - Do any "per frame" actions
+      # tell the ball to update itself
+      # increment each frame
+    
+   # 9 - Clear the window before drawing it again
+    
+    
+    # 10 - Draw the window elements
+       # tell the ball to draw itself
+    
+  
+
+    # 11 - Update the window
+ 
+
+    # 12 - Slow things down a bit
+      # make pygame wait 
